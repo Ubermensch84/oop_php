0fff2dd231de7c087a0e91e16fde96b2b5573ed8
Kirill <llirik_votan@rambler.ru>
2015-07-31 16:15:32 +0300

    Добавил news

---
+++ models/News.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../classes/DB.php';
+
+class News {
+	public $id;
+	public $title;
+	public $text;
+
+	public static function getAll () {
+		$db = new DB;
+		return $db->query('SELECT * FROM news', 'News');
+	}
+
+}
