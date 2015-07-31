4745a478a2a0bf114a3a1b72566c7829c5b6a9d1
Kirill <llirik_votan@rambler.ru>
2015-07-31 15:35:32 +0300

    Обновил инфу

--- classes/DB.php
+++ classes/DB.php
@@ -2,8 +2,20 @@
 
 
 class DB {
-    public function __construct () {
-        mysql_connect('localhost', 'root', '');
-        mysql_select_db('test');
-    }
+	public function __construct () {
+		mysql_connect('localhost', 'root', '');
+		mysql_select_db('test');
+	}
+
+	public function query ($sql) {
+		$res = mysql_query($sql);
+		if (false === $res) {
+			return false;
+		}
+		$ret = [];
+		while ($row = mysql_fetch_object($res)) {
+			$ret[] = $row;
+		}
+		return $ret;
+	}
 }
